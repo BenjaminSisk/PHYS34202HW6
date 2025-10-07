f778b08b866c7151dd3f4d44b202d7f3115a054e
BenjaminSisk <siskb@purdue.edu>
2025-10-07 11:13:41 -0400

    Finished hw6

--- hw6.py
+++ hw6.py
@@ -1,13 +1,44 @@
 import math as m
+import matplotlib.pyplot as plt
 
+# Arbitrary variables
 A = 1
-x = 1 # change to vary over time
-w = 1 # change to be realistic 
-t = 1 # change to be variable
+w = 1 
 
-term1 = m.pow(m.cos(x), 2) * m.exp(-1*m.pow(x,2))
-term2 = m.pow(m.sin(2*x), 2) * m.exp(-0.25*m.pow(x,2))
-term3 = 2 * m.cos(w*t) * m.cos(x) * m.sin(2*x) * m.exp(-0.625*m.pow(x,2))
+xrange = [-2, -1.5, 0, 0.5, 2] # Points in space to investigate on graph - note that -2 and 2 lead to the same result
 
-relativeProbability = 0.5 * m.pow(A, 2) * (term1 + term2 + term3)
-print(relativeProbability)
+tplot = []
+for i in range (m.ceil(10 * w * 2 * m.pi * 3)): # We do ten samples per unit i, and it takes 2piw seconds for one oscillation. We want three oscillations.
+    tplot.append(i/10)
+
+for x in xrange:
+    y = []
+    for t in tplot:
+        # This equation comes directly from what I derived on the LaTeX paper
+        term1 = m.pow(m.cos(x), 2) * m.exp(-1*m.pow(x,2))
+        term2 = m.pow(m.sin(2*x), 2) * m.exp(-0.25*m.pow(x,2))
+        term3 = 2 * m.cos(w*t) * m.cos(x) * m.sin(2*x) * m.exp(-0.625*m.pow(x,2))
+        relativeProbability = 0.5 * m.pow(A, 2) * (term1 + term2 + term3) 
+        y.append(relativeProbability)
+    # Plot each graph of relative probability density versus time individually 
+    plt.plot(tplot, y)
+    plt.show()
+
+'''
+# Sanity check - We are still sloshing even if each point moves up and down at a constant frequency
+trange = [0, m.pi/2, m.pi, 3*m.pi/2, 2 * m.pi] # Note the symmetry across time
+
+xplot = []
+for i in range (-10000, 10001):
+    xplot.append(i/1000)
+for t in range(1, 6):
+    y = []
+    for x in xplot:
+        term1 = m.pow(m.cos(x), 2) * m.exp(-1*m.pow(x,2))
+        term2 = m.pow(m.sin(2*x), 2) * m.exp(-0.25*m.pow(x,2))
+        term3 = 2 * m.cos(w*t) * m.cos(x) * m.sin(2*x) * m.exp(-0.625*m.pow(x,2))
+        relativeProbability = 0.5 * m.pow(A, 2) * (term1 + term2 + term3)
+        y.append(relativeProbability)
+    plt.plot(xplot, y)
+    plt.show()
+'''
